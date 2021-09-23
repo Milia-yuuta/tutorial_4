ffea0379ac643a7622ccc9fcad6438aaa07cb735
cherry <sakurakun22@gmail.com>
2021-09-23 21:49:29 +0900

    [add]11章まで

---
+++ app/helpers/acount_actibations_helper.rb
@@ -0,0 +1,2 @@
+module AcountActibationsHelper
+end
---
+++ test/controllers/acount_actibations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AcountActibationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
